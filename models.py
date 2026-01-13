ddb85da760d18f1148438439966e3d50c87c1361
madhusagunthala <madhushreekathir321@gmail.com>
2026-01-13 19:24:33 +0530

    travello

--- models.py
+++ models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class article1(models.Model):
+    icon = models.CharField(max_length=50)
+    head = models.CharField(max_length=100)
+    para = models.CharField(max_length=300)
+    link = models.CharField(max_length=50)
+    
+class article2(models.Model):
+    img = models.ImageField(upload_to="pic/")
+    head = models.CharField(max_length=100)
+    para = models.CharField(max_length=300)
+    offer = models.BooleanField(default=False)
+    
--- views.py
+++ views.py
@@ -1,47 +1,11 @@
-from django.shortcuts import render,redirect
-from django.http import HttpResponse
-from django.contrib import messages
-from django.contrib.auth.models import User,auth
+from django.shortcuts import render
+from .models import*
 # Create your views here.
-def login(request):
-    if request.method=="POST":
-        username=request.POST['username']
-        password=request.POST['password']
-        user=auth.authenticate(username=username,password=password)
-        if user is not None:
-            auth.login(request,user)
-            return redirect('/')
-        else:
-            messages.info(request,'Invalid credential')
-            return redirect('login')
-    else:
-        return render(request, 'login.html')
-def register(request):
-    if request.method=='POST':
-        first_name=request.POST['first_name']
-        last_name=request.POST['last_name']
-        user_name=request.POST['user_name']
-        email=request.POST['email']
-        password1=request.POST['password1']
-        password2=request.POST['password2']
-        if(password1==password2):
-            if User.objects.filter(username=user_name).exists():
-                messages.info(request, "User Taken")
-                return redirect('register')
-            elif User.objects.filter(email=email).exists():
-                messages.info(request, "Email Taken")
-                return redirect('register')
-            else:
-                user=User.objects.create_user(username=user_name,password=password1,email=email,first_name=first_name,last_name=last_name)
-                user.save()
-                print("User created")
-                return redirect('login')
-        else:
-            messages.info(request, "Password not match")
-            return redirect('register')
-    else:    
-        return render(request, 'register.html')
-    
-def logout(request):
-    auth.logout(request)
-    return redirect('/')
+
+def index(request):
+    arts=article1.objects.all()
+    pics=article2.objects.all()
+    return render(request,'index.html',{
+        'arts': arts,
+        'pics':pics
+        })
